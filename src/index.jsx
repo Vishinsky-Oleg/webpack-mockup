29f720fa659103f421b216c14814d9c480563cb4
Vishinsky-Oleg <vishinsky.oleg@mail.com>
2020-12-24 02:29:20 +0300

    Third commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import ReactDOM, { render } from "react-dom";
+// import React, { Component } from "react";
+// import ReactDOM, { render } from "react-dom";
 import "./styles/style.scss"; //Imports css file
 // import "./styles/styles.css"; //Imports css file
 // import json from "./assets/file"; //ny default imports json file wihout any requests (import understands js and json extensions without explicit declaring)
